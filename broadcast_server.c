ef68cc889f2e2a5dbc3e29a8ec5183130b8f1cef
Allen Liu <RuofanLiu@users.noreply.github.com>
2019-04-13 23:47:19 -0400

    Servercode changed
    server now is able to send valid command to the client

--- broadcast_server.c
+++ broadcast_server.c
@@ -6,7 +6,23 @@
 #include <string.h>     /* for memset() */
 #include <unistd.h>     /* for close() */
 
-//void DieWithError(char *errorMessage);  /* External error handling function */
+/*
+	Supported Command:
+	1. ADD <filename>
+	2. REMOVE <filename>
+*/
+
+/*
+This function checks if a string contains a valid command or not
+It returns the substring before first whitespace
+*/
+char* checkStr(char* command){
+	char *p = strtok(command, " ");
+	if(!p){
+		*p = 0;
+	}
+	return p;
+}
 
 int main(int argc, char *argv[])
 {
@@ -14,19 +30,20 @@ int main(int argc, char *argv[])
     struct sockaddr_in broadcastAddr; /* Broadcast address */
     char *broadcastIP;                /* IP broadcast address */
     unsigned short broadcastPort;     /* Server port */
-    char *sendString;                 /* String to broadcast */
+    char *sendString = (char*)malloc(1024*sizeof(char));                 /* String to broadcast */
     int broadcastPermission;          /* Socket opt to set permission to broadcast */
     unsigned int sendStringLen;       /* Length of string to broadcast */
+	int number = 0;
 
-    if (argc < 4)                     /* Test for correct number of parameters */
+    if (argc < 3)                     /* Test for correct number of parameters */
     {
-        fprintf(stderr,"Usage:  %s <IP Address> <Port> <Send String>\n", argv[0]);
+        fprintf(stderr,"Usage:  %s <IP Address> <Port> \n", argv[0]);
         exit(1);
     }
 
     broadcastIP = argv[1];            /* First arg:  broadcast IP address */ 
     broadcastPort = atoi(argv[2]);    /* Second arg:  broadcast port */
-    sendString = argv[3];             /* Third arg:  string to broadcast */
+    //sendString = argv[3];             /* Third arg:  string to broadcast */	//TO DO: change the stringt be a message
 
     /* Create socket for sending/receiving datagrams */
     if ((sock = socket(PF_INET, SOCK_DGRAM, IPPROTO_UDP)) < 0)
@@ -44,20 +61,29 @@ int main(int argc, char *argv[])
     broadcastAddr.sin_addr.s_addr = inet_addr(broadcastIP);/* Broadcast IP address */
     broadcastAddr.sin_port = htons(broadcastPort);         /* Broadcast port */
 
-    sendStringLen = strlen(sendString);  /* Find length of sendString */
+    
     printf("Starting UDP server for broadcasting\n");
-    for (;;) /* Run forever */
+    while(1) /* Run forever */
     {
+    	/*take input command*/
+    	printf("Type in a number \n");
+		fgets(sendString, 1024, stdin);
+		sendStringLen = strlen(sendString);  /* Find length of sendString */
+		char* cmd = checkStr(sendString);
+		if(strcmp(cmd, "ADD") == 0 || strcmp(cmd, "RM") == 0){
          /* Broadcast sendString in datagram to clients every 3 seconds*/
-        if (sendto(sock, sendString, sendStringLen, 0, (struct sockaddr *) 
-               &broadcastAddr, sizeof(broadcastAddr)) != sendStringLen){
-             perror("sendto() sent a different number of bytes than expected");
-     	}
-     	else{
-     		printf("Sending message to all clients: %s\n", sendString);
-     	}
+		
+	        if (sendto(sock, sendString, sendStringLen, 0, (struct sockaddr *) 
+	               &broadcastAddr, sizeof(broadcastAddr)) != sendStringLen){
+	             perror("sendto() sent a different number of bytes than expected");
+	     	}
+	     	else{
+	     		printf("Sending message to all clients: %s\n", sendString);
+	     	}
 
-        sleep(3);   /* Avoids flooding the network */
+	        sleep(3);   /* Avoids flooding the network */
+     	}
     }
     /* NOT REACHED */
 }
+
